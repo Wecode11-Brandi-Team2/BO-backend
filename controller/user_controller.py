92caf3c1c279cd35428c738bd201df6754320c03
haejun <hj885353@gmail.com>
2020-09-25 20:57:04 +0900

    json return을 dao -> controller 로 수정 및 주석 정리

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -5,16 +5,27 @@ from flask import (
 
 def create_user_endpoints(services, Session):
     user_service = services
-    user_bp = Blueprint('user', __name__, url_prefix = '/user')
+    user_bp = Blueprint('user', __name__, url_prefix = '/api/user')
 
     @user_bp.route('/', methods = ['GET'])
+    """
+    Args:
+        services : service.py 연결
+        Session : db 연결
+    Return:
+        user_list dictionary 형태로 반환
+    Authors:
+        hj885353@gmail.com(김해준)
+    History;
+        2020-09-21 (hj885353@gmail.com) : 초기 생성
+    """
     def user_info():
         try:
             session = Session()
 
             user_info = user_service.get_user_info_service(session)
 
-            return user_info
+            return jsonify({'user_list' : [ dict(user) for user in user_info ]})
 
         except Exception as e:
             return jsonify({'ERROR_MSG' : f'{e}' }), 500
--- service/user_service.py
+++ service/user_service.py
@@ -3,6 +3,7 @@ class UserService:
         self.user_dao = user_dao
 
     def get_user_info_service(self, session):
+        # dao와 controller 연결해주는 함수
         user_info = self.user_dao.get_user_info(session)
         
         return user_info

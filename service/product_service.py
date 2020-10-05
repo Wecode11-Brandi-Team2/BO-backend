3660f8b980f8117295da21ff474a4b88627f6808
JiwonKo <rhwldnjs1226@gmail.com>
2020-10-05 23:00:22 +0900

    [상품 정보 등록 API / 셀러 리스트 API / 카테고리 리스트 API] - S3 이미지 1개 업로드 구현 - 상품 정보 등록 시 필요한 셀러 정보 전달, 카테고리 리스트 전달 API 구현

--- service/product_service.py
+++ service/product_service.py
@@ -1,6 +1,47 @@
+from config import get_s3_resource
+
 class ProductService:
     def __init__(self, product_dao):
         self.product_dao = product_dao
+
+    def get_first_categories(self, seller_info, session):
+        """ 상품 등록 시 셀러의 속성에 맞는 첫 번째 카테고리 리스트 전달
+
+        Authors:
+            고지원
+
+        History:
+            2020-10-04 (고지원): 초기 생성
+        """
+        first_categories = self.product_dao.get_first_categories(seller_info, session)
+
+        return first_categories
+
+    def get_second_categories(self, seller_info, session):
+        """ 상품 등록 시 첫 번째 카테고리에 해당하는 두 번째 카테고리 리스트 전달
+
+        Authors:
+            고지원
+
+        History:
+            2020-10-04 (고지원): 초기 생성
+        """
+        second_categories = self.product_dao.get_second_categories(seller_info, session)
+
+        return second_categories
+
+    def get_sellers(self, q, session):
+        """ 셀러 데이터 전달
+
+        Authors:
+            고지원
+
+        History:
+            2020-10-04 (고지원): 초기 생성
+        """
+        sellers = self.product_dao.get_sellers(q, session)
+
+        return sellers
 
     def insert_product(self, product_info, session):
         """ 상품 정보 insert
@@ -27,14 +68,32 @@ class ProductService:
 
         return products
 
+    def save_product_image(self, image, product_code):
+        """ s3에 이미지를 업로드
+        Authors:
+            고지원
+
+        History:
+            2020-10-05 (고지원): 초기 생성
+        """
+
+        # s3 서버 연결 및 이미지 업로드
+        s3_resource = get_s3_resource()
+        s3_resource.put_object(Body = image, Bucket = 'brandi-images', Key = f'{product_code}.jpeg', ContentType = 'image/jpeg')
+
+        # 데이터베이스에 저장할 이미지 url
+        image_url = f'https://brandi-images.s3.ap-northeast-2.amazonaws.com/{product_code}.jpeg'
+
+        return image_url
+
     def get_product(self, product_id, session):
-        """ 상품 데이터 전달
+        """ 상품 상세 데이터 전달
 
         Authors:
             고지원
 
         History:
-            2020-09-23 (고지원): 초기 생성
+            2020-10-03 (고지원): 초기 생성
         """
         product = self.product_dao.get_product(product_id, session)
         return product
